c4877d6cca0a351abc9060ba3bd6f7ce470ac83e
tiff <tiffany.dessouroux@gmail.com>
2022-01-31 14:06:26 +0100

    to be continued

--- framer-motion/src/App.js
+++ framer-motion/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Box1 from './components/Box1/Box1';
+import List from './components/List/List';
 
 function App() {
   return (
     <div className="App">
  <Box1/>
+ <List/>
     </div>
   );
 }
